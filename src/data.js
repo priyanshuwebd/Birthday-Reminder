dc11268021a493f4eb5214f7b1839fab5574010a
priyanshupande <priyanshupande23@gmail.com>
2024-12-08 20:28:42 +0530

    correct link

--- src/data.js
+++ src/data.js
@@ -1,27 +1,28 @@
-export default [
-    {
-      id: 1,
-      name: "priyanshi",
-      age: 24,
-      image: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg"
-    },
-    {
-      id: 2,
-      name: "devanshi",
-      age: 24,
-      image: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg"
-    },
-    {
-      id: 3,
-      name: "Ritik",
-      age: 24,
-      image: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg"
-    },
-    {
-      id: 4,
-      name: "himanshi",
-      age: 24,
-      image: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg"
-    }
-  ];
-  
+const people = [
+  {
+    id: 1,
+    name: "priyanshi",
+    age: 24,
+    image: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg"
+  },
+  {
+    id: 2,
+    name: "devanshi",
+    age: 24,
+    image: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg"
+  },
+  {
+    id: 3,
+    name: "Ritik",
+    age: 24,
+    image: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg"
+  },
+  {
+    id: 4,
+    name: "himanshi",
+    age: 24,
+    image: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg"
+  }
+];
+
+export default people;
